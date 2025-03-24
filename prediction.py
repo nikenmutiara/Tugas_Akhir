372000e396d96159e9f38f86ec04540ee47cef2f
niken mutiara wulandari <nknmutiara@gmail.com>
2025-03-24 08:09:31 -0700

    perbaikan

--- prediction.py
+++ prediction.py
@@ -9,9 +9,9 @@ from datetime import datetime
 from sqlalchemy.exc import SQLAlchemyError
 
 # Load model correctly using Keras/TensorFlow (not pickle)
-DO_model = tf.keras.models.load_model('model2.h5')
+DO_model = tf.keras.models.load_model('best_lstm_model3.keras')
 # Load scaler with pickle (this is correct)
-scaler = pickle.load(open('Scaler2.pkl', 'rb'))
+scaler = pickle.load(open('Scaler3.pkl', 'rb'))
 
 # Database connection setup
 DATABASE_URL = "mysql+pymysql://root:@localhost/klasifikasi_do"
